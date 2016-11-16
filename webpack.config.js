ae0d97aade590f1bf1fb1b176366c846deab6992
galperie <11galpel@gmail.com>
2016-11-16 14:54:13 -0500

    Ellen - adding babel and configuring webpack to minify our index.js to a build file

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/public'); // Transpiled and Compiled code
+var APP_DIR = path.resolve(__dirname, 'src/app'); // React App
+
+module.exports = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'index.min.js'
+  }
+};
